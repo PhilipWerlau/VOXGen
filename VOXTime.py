f38dbdc8588de249462edfc33fa93d6c3a243997
Philip Werlau <PhilipWerlau@gmail.com>
2019-08-26 15:45:58 -0400

    Update VOXTime.py

--- VOXTime.py
+++ VOXTime.py
@@ -10,7 +10,7 @@ import geocoder
 SOUND_DIR = 'vox/'
 
 def convertNumber(num):
-    one = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
+    one = ['', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
     tenp = ['Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen']
     tenp2 = ['', '', 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety']
     word = ''
@@ -18,7 +18,10 @@ def convertNumber(num):
     numStr = str(numInt)
     leng = len(numStr)
     if leng == 1:
-        word = one[numInt]
+        if numInt == 0:
+            word = 'Zero'
+        else:
+            word = one[numInt]
     if leng == 2:
         if numStr[0] == '1':
             word = tenp[int(numStr[1])]
@@ -34,7 +37,7 @@ currentHour = datetime.datetime.now().strftime("%H")
 currentMinute = datetime.datetime.now().strftime("%M")
 print(currentHour+":"+currentMinute)
 
-sentence = "The time is " + convertNumber(currentHour) + " hundred hours and " + convertNumber(currentMinute) + " minutes"
+sentence = "doop the time is " + convertNumber(currentHour) + " hundred hours and " + convertNumber(currentMinute) + " minutes"
 
 geo = geocoder.ip('me')
 location = geo.city+","+geo.country
@@ -43,17 +46,16 @@ weatherJson = json.loads(requests.get("http://api.openweathermap.org/data/2.5/we
 temperature = weatherJson['main']['temp']
 sentence += " _comma _comma _comma the topside temperature is " + convertNumber(str(int(temperature))) + " degrees fahrenheit"
 
-
+print(sentence)
 sentence = sentence.lower().split(' ')
 accepted = []
 
-print(sentence)
 
 for i in sentence:
     if os.path.isfile(SOUND_DIR + i + '.wav'):
         accepted.append(i)
 
-print(accepted)
+#print(accepted)
 
 splice = []
 for i in accepted:
@@ -74,11 +76,11 @@ stream.stop_stream()
 stream.close()   
 pAudio.terminate()
 
-#try:
-#    out = wave.open('time.wav', 'wb')
-#    out.setparams(splice[0][0])
-#    for params,frames in splice:
-#        out.writeframes(frames)
-#    out.close()
-#except:
-#    print('FATAL: Failed to write to output file.')
+try:
+    out = wave.open('time.wav', 'wb')
+    out.setparams(splice[0][0])
+    for params,frames in splice:
+        out.writeframes(frames)
+    out.close()
+except:
+    print('FATAL: Failed to write to output file.')
